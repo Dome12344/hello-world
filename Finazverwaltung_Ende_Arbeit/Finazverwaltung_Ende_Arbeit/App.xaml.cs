b9f20a1795759c4647f031776eaa07fe36c2a1fc
Dome12344 <150655714+Dome12344@users.noreply.github.com>
2025-02-05 09:31:26 +0100

    Test

---
+++ Finazverwaltung_Ende_Arbeit/Finazverwaltung_Ende_Arbeit/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Finazverwaltung_Ende_Arbeit
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ Finazverwaltung_Ende_Arbeit/Finazverwaltung_Ende_Arbeit/Budget_Berchnen_und_anzeigen.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Finazverwaltung_Ende_Arbeit
+{
+    /// <summary>
+    /// Interaktionslogik für Budget_Berchnen_und_anzeigen.xaml
+    /// </summary>
+    public partial class Budget_Berchnen_und_anzeigen : Window
+    {
+        public Budget_Berchnen_und_anzeigen()
+        {
+            InitializeComponent();
+        }
+    }
+}
